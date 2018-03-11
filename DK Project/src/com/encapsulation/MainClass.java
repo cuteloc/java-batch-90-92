c84eed76fa65ae3dcf14fdba43f66d3c8554d861
dkupadhyay <dkupadhyay@Daksh>
2018-03-10 20:39:24 -0700

    updating

--- DK Project/src/com/encapsulation/MainClass.java
+++ DK Project/src/com/encapsulation/MainClass.java
@@ -3,7 +3,7 @@ package com.encapsulation;
 public class MainClass {
 	public static void main(String[] args) {
 		AccessSpecifiersDemo d1=new AccessSpecifiersDemo();
-		System.out.println(d1.);
+		System.out.println(d1.b);
 	}
 
 }
